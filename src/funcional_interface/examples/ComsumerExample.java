8e78a6e7e30a03cefc4de99b827602840bf69740
Pedro Henrique <pedrouser9@gmail.com>
2023-08-24 11:39:25 -0300

    ConsumerExample

---
+++ src/funcional_interface/examples/ComsumerExample.java
@@ -0,0 +1,34 @@
+package funcional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ComsumerExample {
+
+	public static void main(String[] args) {
+
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+		/*
+		 * Usando fora Consumer<Integer> imprimirNumeroPar = numero -> { if (numero % 2
+		 * == 0) { System.out.println(numero); } };
+		 */
+		// Usando Consumer direto
+		numeros.stream().forEach(new Consumer<Integer>() {
+
+			public void accept(Integer n) {
+				if (n % 2 == 0) {
+					System.out.println(n);
+				}
+			}
+		});
+
+		// Usando lambda
+		numeros.forEach(n -> {
+			if (n % 2 == 0) {
+				System.out.println(n);
+			}
+		});
+
+	}
+}
