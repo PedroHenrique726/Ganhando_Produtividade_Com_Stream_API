bd7bd04229ef29a1a319bae529da0839fd325ee1
Pedro Henrique <pedrouser9@gmail.com>
2023-08-24 12:53:02 -0300

    SupplierExample

---
+++ src/funcional_interface/examples/SupplierExample.java
@@ -0,0 +1,20 @@
+package funcional_interface.examples;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+
+	public static void main(String[] args) {
+		
+		Supplier<String> saudacao = () -> "Ola, seja bem vindo";
+		
+		List<String> listaSaudacoes = Stream.generate(() -> "Ola, seja bem vindo")
+				.limit(5)
+				.toList();
+
+		listaSaudacoes.forEach(System.out::println);
+	}
+}
+
